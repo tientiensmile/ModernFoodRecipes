44143e1b931627a5bda25ac3dd32e323185a4f8c
audrey_wang@cmoney.com.tw <audrey_wang@cmoney.com.tw>
2022-08-23 23:13:02 +0800

    feat: 新增mainTabBar

--- ModernFoodRecipes/app/src/main/java/com/example/modernfoodrecipes/MainActivity.kt
+++ ModernFoodRecipes/app/src/main/java/com/example/modernfoodrecipes/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.modernfoodrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setNavController()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setNavController() {
+        navController = findNavController(R.id.navHostFragment)
+        var appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        var bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 }
